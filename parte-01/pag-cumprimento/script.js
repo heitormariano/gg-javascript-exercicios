6e70f463fe8989c5db20db2cd42c8eeb86a340e4
Heitor Mariano <heitor.eumesmo@gmail.com>
2020-04-09 22:02:14 -0300

    files updated

--- parte-01/pag-cumprimento/script.js
+++ parte-01/pag-cumprimento/script.js
@@ -1,1 +1,24 @@
+function carregar() {
+    let msg = window.document.getElementById('mensagem');
+    let img = window.document.getElementById('imagem');
 
+    let data = new Date();
+    let hora = data.getHours();
+
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        msg.innerHTML = `Hora Atual: ${hora}. Bom dia!`;
+        img.src = './imgs/manha_recortada.png';
+        document.body.style.background = '#fdd246';
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde
+        msg.innerHTML = `Hora Atual: ${hora}. Boa tarde!`;
+        img.src = './imgs/tarde_recortada.png';
+        document.body.style.background = '#f9b275';
+    } else {
+        // boa noite
+        msg.innerHTML = `Hora Atual: ${hora}. Boa noite!`;
+        img.src = './imgs/noite_recortada.png';
+        document.body.style.background = '#784772';
+    }
+}
